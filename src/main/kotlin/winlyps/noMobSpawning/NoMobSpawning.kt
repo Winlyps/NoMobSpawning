9e79cc62bad95e4f3cf503b9336bebaf9a3b227e
Winlypse <nts3@o2.pl>
2024-10-22 15:05:11 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noMobSpawning/NoMobSpawning.kt
@@ -0,0 +1,14 @@
+package winlyps.noMobSpawning
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoMobSpawning : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
